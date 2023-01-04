b721d3a819b0201501005d049fdb2490cdd803e7
Daniel Tompkins <dwtompkins@sei.cmu.edu>
2023-01-04 10:27:41 -0500

    I think null-ls is working?

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,41 +1,56 @@
-local present, null_ls = pcall(require, "null-ls")
-
-if not present then
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
   return
 end
 
-local b = null_ls.builtins
+local M = {}
 
-local sources = {
-  -- format html and markdown
-  --b.formatting.prettierd.with { filetypes = { "html", "yaml", "markdown" } },
-  -- markdown diagnostic
-  --b.diagnostics.markdownlint,
-  -- Lua formatting
-  --b.formatting.stylua,
-  -- django
-  b.formatting.djlint
-}
-
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local on_attach = function(client, bufnr)
-  if client.supports_method "textDocument/formatting" then
-    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-        -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-        vim.lsp.buf.formatting_sync()
-        end,
-    })
-  end
-end
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local formatting = null_ls.builtins.formatting
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup {
-  debug = true,
-  autostart = true,
-  sources = sources,
-  on_attach = on_attach,
+  debug = false,
+  sources = {
+    formatting.prettier.with {
+      extra_filetypes = { "toml", "solidity" },
+      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    },
+    diagnostics.djlint,
+    formatting.gofumpt,
+    formatting.autopep8,
+    formatting.stylua,
+    formatting.clang_format.with {
+      filetypes = { "cpp", "c" },
+    },
+  },
+  border = 'rounded',
 }
 
+function M.list_registered_providers_names(filetype)
+  local s = require "null-ls.sources"
+  local available_sources = s.get_available(filetype)
+  local registered = {}
+  for _, source in ipairs(available_sources) do
+    for method in pairs(source.methods) do
+      registered[method] = registered[method] or {}
+      table.insert(registered[method], source.name)
+    end
+  end
+  return registered
+end
+
+function M.list_registered_formatters(filetype)
+  local registered_providers = M.list_registered_providers_names(filetype)
+  local method = null_ls.methods.FORMATTING
+  return registered_providers[method] or {}
+end
+
+function M.list_registered_linters(filetype)
+  local registered_providers = M.list_registered_providers_names(filetype)
+  local method = null_ls.methods.DIAGNOSTICS
+  return registered_providers[method] or {}
+end
+
+return M
