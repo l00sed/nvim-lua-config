21e734daf2e666a9f3c8e5bf8609892d3f26df90
l00sed <dwtompkins@sei.cmu.edu>
2024-08-20 15:40:17 -0400

    Updated barbecue lua to get right icons.

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -1,4 +1,5 @@
 require('barbecue').setup({
+  create_autocmd = false,
   theme = {
     normal = { bg = 'None' },
   },
@@ -36,5 +37,21 @@ require('barbecue').setup({
     Event = "",
     Operator = "",
     TypeParameter = "",
-  },
+  }
 })
+
+vim.api.nvim_create_autocmd({
+  "WinScrolled", -- or WinResized on NVIM-v0.9 and higher
+  "BufWinEnter",
+  "CursorHold",
+  "InsertLeave",
+
+  -- include this if you have set `show_modified` to `true`
+  "BufModifiedSet",
+}, {
+  group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+  callback = function()
+    require("barbecue.ui").update()
+  end
+})
+
