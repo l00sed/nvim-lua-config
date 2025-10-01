800d67157d36b5d2a38878847fdfb6f3c05039ba
l00sed <dwtompkins@sei.cmu.edu>
2025-10-01 00:02:10 -0400

    trying out the opencode nvim client

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -117,56 +117,26 @@ return {
   },
 
   {
-    'NickvanDyke/opencode.nvim',
+    "sudo-tee/opencode.nvim",
+    config = function()
+      require("opencode").setup()
+    end,
     dependencies = {
-      -- Recommended for better prompt input, and required to use opencode.nvim's embedded terminal. Otherwise optional.
-      { 'folke/snacks.nvim', opts = { input = { enabled = true } } },
-    },
-    ---@type opencode.Opts
-    opts = {
-      -- Your configuration, if any
-    },
-    keys = {
-      -- Recommended keymaps
-      { '<leader>oA', function() require('opencode').ask() end, desc = 'Ask opencode', },
-      { '<leader>oa', function() require('opencode').ask('@cursor: ') end, desc = 'Ask opencode about this', mode = 'n', },
-      { '<leader>oa', function() require('opencode').ask('@selection: ') end, desc = 'Ask opencode about selection', mode = 'v', },
-      { '<leader>ot', function() require('opencode').toggle() end, desc = 'Toggle embedded opencode', },
-      { '<leader>on', function() require('opencode').command('session_new') end, desc = 'New session', },
-      { '<leader>oy', function() require('opencode').command('messages_copy') end, desc = 'Copy last message', },
-      { '<S-C-u>',    function() require('opencode').command('messages_half_page_up') end, desc = 'Scroll messages up', },
-      { '<S-C-d>',    function() require('opencode').command('messages_half_page_down') end, desc = 'Scroll messages down', },
-      { '<leader>op', function() require('opencode').select_prompt() end, desc = 'Select prompt', mode = { 'n', 'v', }, },
-      -- Example: keymap for custom prompt
-      { '<leader>oe', function() require('opencode').prompt("Explain @cursor and its context") end, desc = "Explain code near cursor", },
-    },
+      "nvim-lua/plenary.nvim",
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          anti_conceal = { enabled = false },
+          file_types = { 'markdown', 'opencode_output' },
+        },
+        ft = { 'markdown', 'Avante', 'copilot-chat', 'opencode_output' },
+      },
+      -- Optional, for file mentions and commands completion, pick only one
+      'hrsh7th/nvim-cmp',
+      -- Optional, for file mentions picker, pick only one
+      'nvim-telescope/telescope.nvim'
+    }
   },
-
-  -- GitHub Copilot
-  --{
-  --  "zbirenbaum/copilot.lua",
-  --  cmd = "Copilot",
-  --  event = "InsertEnter",
-  --  config = function()
-  --    require("plugins.copilot")
-  --  end
-  --},
-
-  -- nvim-cmp integration for Copilot
-  --{
-  --  "zbirenbaum/copilot-cmp",
-  --  config = function ()
-  --    require("copilot_cmp").setup()
-  --  end
-  --},
-
-  -- AI code helper
-  --{
-  --  "milanglacier/minuet-ai.nvim",
-  --  config = function()
-  --    require("plugins.minuet")
-  --  end
-  --},
 
   -- Image Viewer
   {
