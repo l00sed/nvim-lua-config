450ead4b1f9de1fb309f4c4d093a570f5622051f
l00sed <dwtompkins@sei.cmu.edu>
2026-03-23 10:56:46 -0400

    update: Adding the LSP source to virtual line

--- lua/plugins/lsplines.lua
+++ lua/plugins/lsplines.lua
@@ -1,9 +1,23 @@
 -- Disable virtual_text since it's redundant due to lsp_lines.
 vim.diagnostic.config({
-  virtual_text = false,
-  -- To disable, uncomment the line below
-  --virtual_lines=false
   virtual_lines = {
     only_current_line = true
-  }
+  },
+  virtual_text = {
+    source = true,
+    current_line = true,
+    format = function(diagnostic)
+      if diagnostic.user_data and diagnostic.user_data.code then
+        return string.format('%s %s', diagnostic.user_data.code, diagnostic.message)
+      else
+        return diagnostic.message
+      end
+    end,
+  },
+  signs = true,
+  float = {
+    header = 'Diagnostics',
+    source = true,
+    border = 'rounded',
+  },
 })
