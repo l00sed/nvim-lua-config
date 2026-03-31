e478a106469934d1bdc48652c5297eee75e219f1
l00sed <danielwtompkins@gmail.com>
2026-03-30 20:38:26 -0400

    update: Adding outline plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -201,6 +201,24 @@ return {
     end,
   },
 
+  -- Outline sidebar
+  {
+    "hedyhli/outline.nvim",
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+      require('outline').setup({
+        providers = {
+          priority = { 'lsp', 'coc', 'markdown', 'norg', 'treesitter' },
+        },
+      })
+    end,
+    event = "VeryLazy",
+    dependencies = {
+      'epheien/outline-treesitter-provider.nvim'
+    }
+  },
+
   -- FZF Native
   {
     "nvim-telescope/telescope-fzf-native.nvim",
